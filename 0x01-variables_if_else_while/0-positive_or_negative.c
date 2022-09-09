35db327c206065f15c31687c7690952d89b181f1
Okello Richard <okellor2@gmail.com>
2022-09-09 10:45:04 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
+
 /* betty stue doc for function main goes there */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
