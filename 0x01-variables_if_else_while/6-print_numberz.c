1073027ae41f507076f349db74109c60c18f80bf
Okello Richard <okellor2@gmail.com>
2022-09-09 23:42:13 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program prints all the digit in base 10 start with 0
+ * You are allowed to use any variables char
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n += 1;
+	}
+	printf("\n");
+	return (0);
+}
