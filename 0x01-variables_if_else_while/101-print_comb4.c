5eef61d888dbceb885ba93fd4ded09333629add5
Okello Richard <okellor2@gmail.com>
2022-09-10 01:16:25 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-  
- /** 
-  * main - program that prints all possible different combinations of 
-  * three digits 
-  * Numbers must be separated by ,followed by a space 
-  * The two digits must be different 
-  * 012, 120, 102, 021, 201, 210 are considered the same combination 
-  * Return: 0 
-  */ 
+/**
+ * main - program that prints all possible combination
+ * numbers must be seoerated by ,
+ * the two digits must be different 
+ * 012, 0120, 102, 021, 201, 210 arw one combi ation
+ * Return: 0
+ */
   
  int main(void)
- { 
+ {
          int n1 = 48; 
          int n2, n3; 
          int com = 44; 
