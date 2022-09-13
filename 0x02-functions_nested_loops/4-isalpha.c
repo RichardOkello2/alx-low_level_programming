fc0330bf6e46f61bd09945017385d70a68895500
Okello Richard <okellor2@gmail.com>
2022-09-13 11:26:17 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * Rwturn: 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
-
+/* Return: 1 if c is alphabet */
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
