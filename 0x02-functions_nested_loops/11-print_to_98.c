7e5e349696dec5828e50f96e162bfd5ef71fc16c
Okello Richard <okellor2@gmail.com>
2022-09-14 11:11:35 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,7 @@
  * @n: The number to start printing from
  * Return: Always 0.
  */
+
 void print_to_98(int n)
 {
 	if (n <= 98)
@@ -14,8 +15,8 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d", n);
-			       printf("\n");
-			       break;
+				printf("\n");
+				break;
 			}
 			else
 			{
