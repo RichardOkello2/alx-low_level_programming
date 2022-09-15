bdb33fccdb52d6c45e50d31acc6b85714a5564a4
Okello Richard <okellor2@gmail.com>
2022-09-15 08:23:09 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper - Check if a letter id upper
- * @x: The number to be checked
+ * _isupper - Check if a letter is upper
+ * @c: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 int _isupper(int c)
