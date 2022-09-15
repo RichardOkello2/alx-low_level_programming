5653073c92aa4a115e77be8a4d51a5d2f81acf32
Okello Richard <okellor2@gmail.com>
2022-09-15 11:36:02 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 0 to 14
+ * more_numbers - prints 0 - 14
  * Return: void
  */
 
@@ -10,7 +10,7 @@ void more_numbers(void)
 	char n, c;
 	int i = 0;
 
-	while(i < 10)
+	while (i < 10)
 	{
 		for (n = 0; n <= 14; n++)
 		{
