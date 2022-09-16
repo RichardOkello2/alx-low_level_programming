b57cae270192c8fdc965de7ecd139b9e54dfd567
Okello Richard <okellor2@gmail.com>
2022-09-16 12:53:59 -0700

    Task10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,38 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Fizz Buzz code
- * Return: void
+ * main - pri tsa Fizz Buzz program
+ * Return: 0
  */
-
 int main(void)
 {
-	i = 1;
+	int x;
 
-	while (i <= 100)
+	for (x = 1; x <= 100; x++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 3 == 0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (x % 5 == 0)
 		{
 			printf("Buzz");
 		}
 		else
 		{
-			printf("%i", i);
+			printf("%d", x);
 		}
-		if (i <= 100)
+		if (x != 100);
 		{
-			putchar(' ');
+			printf(" ");
 		}
-		i++;
 	}
-	putchar('\n');
-	return (0);
-}
+	printf("\n");
+	
+	return (0); 
+ }
