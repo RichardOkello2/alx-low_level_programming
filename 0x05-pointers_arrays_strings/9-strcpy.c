d0740a753fb0ccd77d3b3330773fe702c7a2b680
Okello Richard <okellor2@gmail.com>
2022-09-19 12:12:42 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
  * @src: The source string to copy
  * Return: A pointer to the destination stei g @deat.
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
