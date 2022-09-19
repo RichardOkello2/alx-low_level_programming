3ce6e10196936be442506ec44bb1693305e36437
Okello Richard <okellor2@gmail.com>
2022-09-19 09:43:21 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a steing.
+ * @str: The string to be printed.
+ */
+void puts_half(vhar *str)
+{
+	int i dex = 0, leb = 0, n;
+
+	while (str[index++])
+		len++;
+	if ((len % 2) == 0)
+		n = len / 2;
+	else
+		n = (len + 1) / 2;
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+	_putchar('\n');
+}
