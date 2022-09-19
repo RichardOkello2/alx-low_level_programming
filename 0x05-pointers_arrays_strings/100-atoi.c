7eadfcaf8cdb0277c6a790deb4c57a4d30e57342
Okello Richard <okellor2@gmail.com>
2022-09-19 12:36:04 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,7 +2,7 @@
 /**
  * _atoi - Converts a string to an integer.
  * @s: The string to be converted
- * Return; The integer value of the converted string.
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
