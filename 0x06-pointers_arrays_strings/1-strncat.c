b7c85448aa6f2c6f8402397d743de14d47afea28
Okello Richard <okellor2@gmail.com>
2022-09-21 12:25:41 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,9 +15,9 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	while (src[j] != src[n])
 	{
-	dest[i] = src[j];
-i++
-j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
