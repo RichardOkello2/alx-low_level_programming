d78f42694f10e33ede510c5c7784c0e50ab827a3
Okello Richard <okellor2@gmail.com>
2022-09-21 03:37:42 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - Fills the first n bytes of the memory area
+ * pointed to by @s wuth the constant byte @c.
+ * @s: A pointer to the memory area to be filled.
+ * The character to fill the memory area with.
+ * @c: The charater to filled the memory area.
+ * @n: The number of bytes to be filled.
+ * Return: A pointer to the fillrd memory area @s.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+	return (memory);
+}
