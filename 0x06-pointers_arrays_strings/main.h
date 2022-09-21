b44be9422cb4a2e83c7d559330896f66171dbaa1
Okello Richard <okellor2@gmail.com>
2022-09-21 08:21:23 -0700

    Task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
