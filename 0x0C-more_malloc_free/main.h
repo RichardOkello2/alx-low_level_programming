2920550395e53211139d7636c650a8dae2443047
Okello Richard <okellor2@gmail.com>
2022-10-07 08:37:04 -0700

    Task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-void *malloc_checked(unsiged int b);
+void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
