3c40577a35e7f52d9614a00934589ac458e6c561
Okello Richard <okellor2@gmail.com>
2022-10-10 12:17:49 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef HEADER
 #define HEADER
 /**
- * struct dog - Dog attributes
+ * struct dog - Dog attributes
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- * Description: The attributes of a dog.
+ * struct dog: The attributes of a dog.
  */
 struct dog
 {
