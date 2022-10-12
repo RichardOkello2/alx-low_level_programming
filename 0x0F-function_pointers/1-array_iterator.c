629a465be19fab5175bd4865b95d49b9ed6ea10e
Okello Richard <okellor2@gmail.com>
2022-10-12 04:10:15 -0700

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/* defines size_t */
+/**
+ * array_iterator - execute function on an array via a function ptr
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function that we need to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if (arry != NULL && action != NULL)
+		for (i = 0; i < size; i++)
+			action(*(array + i));
+}
