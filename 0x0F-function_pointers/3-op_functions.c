06c60fee2057ceda9ce98ad2ef1a73725ab869f0
Okello Richard <okellor2@gmail.com>
2022-10-12 11:53:44 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - ...
+ * op_sub - ...
+ * op_mul - ...
+ * op_div - ...
+ * op_mod - ...
+ * @a: ...
+ * @b: ...
+ * Return: ...
+ */
+int op-add(int a, int b)
+{
+	return (a + b);
+}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+int op_div(int a, int b);
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
