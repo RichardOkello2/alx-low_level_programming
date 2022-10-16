9c7676fa582506174548f4a61696fafda4d81ffc
Okello Richard <okellor2@gmail.com>
2022-10-16 01:53:59 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,18 +9,18 @@
  * @owner: The owner of the dog.
  * struct dog: The attributes of a dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 /**
  * dog_t - Typedef for dog structure
  */
-typedef struct dog dog_t;
-void init_dog(struct dog *d, cgar *name, float age, char *owner);
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(cgar *name, float age, cgar *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
