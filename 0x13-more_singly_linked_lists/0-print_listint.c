75feeb5bad12cd6f61079adfac35a62d19a28e83
Okello Richard <okellor2@gmail.com>
2022-10-24 11:12:34 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
  * @h: The head of the linked list
  * Return: The number of nodes of the linked list
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
         int count = 0;
 
@@ -13,7 +13,7 @@ size_t print_listint(const listint_t *h)
         {
                 while (h)
                 {
-                        printf("%d\n", h->n);
+                        printf("%d\n", h->n);
                         h = h->next;
                         count++;
                 }
